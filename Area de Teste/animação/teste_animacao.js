817518374aa90e13b1e7c11b8c9a09b7cf42872f
Tiago Norberto de Andrade Junior <79945244+Tiijunior@users.noreply.github.com>
2023-12-23 00:26:23 -0300

    Delete Area de Teste directory

--- Area de Teste/animação/teste_animacao.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const content = document.getElementById("content");
-  
-    setTimeout(function() {
-      content.classList.remove("animate-out");
-    }, 1000);
-  });
-  
